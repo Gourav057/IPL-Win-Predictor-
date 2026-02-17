3569d89947e5a5cdbc4103d81fb3e285887514b8
Gourav <gouravbhardwaj2004@gmail.com>
2026-02-17 20:00:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pickle
+
+teams = [
+    'Sunrisers Hyderabad',
+    'Mumbai Indians',
+    'Royal Challengers Bangalore',
+    'Kolkata Knight Riders',
+    'Kings XI Punjab',
+    'Chennai Super Kings',
+    'Rajasthan Royals',
+    'Delhi Capitals'
+]
+
+cities = [
+    'Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
+    'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
+    'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
+    'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
+    'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
+    'Sharjah', 'Mohali', 'Bengaluru'
+]
+
+pipe = pickle.load(open('pipe.pkl', 'rb'))
+
+st.title("IPL Win Predictor")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    batting_team = st.selectbox('Select the batting team', teams)
+
+with col2:
+    bowling_team = st.selectbox('Select the bowling team', teams)
+
+
